cbc6d5a560ce96f6dc9a4b2c1e3150f057b548c2
404lucas <404lucas.ferreira@gmail.com>
2023-08-22 11:38:36 -0300

    filé!

--- classes/connectionFactory.php
+++ classes/connectionFactory.php
@@ -13,10 +13,11 @@ class connectionFactory
     {
         if (self::$pdo == null) {
             try {
-                self::$pdo = new PDO('mysql:host=' . HOST . ';dbname=' . DATABASE, USER, PASSWORD, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+                self::$pdo = new PDO("mysql:host=" . HOST . ";dbname=" . DATABASE, USER, PASSWORD, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
                 self::$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                echo 'Conectado!';
             } catch (Exception $e) {
-                echo frontend::alert('times', 'danger', '<b>Falha catastrófica</b> na conexão com o banco de dados!');
+                echo "{$e}";
             }
         }
         return self::$pdo;
